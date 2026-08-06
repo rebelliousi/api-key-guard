ebbcb4f9f30b674eccf70c517d26bf80a663565e
rebelliousi <learneryolo3@gmail.com>
2026-08-06 11:44:40 +0500

    feat: add global ApiKeyGuard with header-based auth and .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ApiKeyGuard } from './api-key/api-key.guard';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ApiKeyGuard,
+    },
+  ],
 })
 export class AppModule {}
